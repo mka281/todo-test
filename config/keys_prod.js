a23729c92253ce687e654b8d4abff8fabf061589
Muhammed Kilicarslan <mka281@icloud.com>
2019-12-13 11:42:43 -0500

    add missing key names to keys.prod

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
+  awsAccessKey: process.env.AWS_ACCESS,
+  awsSecretKey: process.env.AWS_SECRET
 };
